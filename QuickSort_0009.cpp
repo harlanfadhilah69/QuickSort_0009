d03041d2c0e0fd2b4c21da643de71adc2edb0f79
harlanfadhilah69 <harlancoegg@gmail.com>
2024-03-28 16:50:56 +0700

    void input : while

--- QuickSort_0009.cpp
+++ QuickSort_0009.cpp
@@ -7,5 +7,15 @@ int mov_count = 0;
 int n;
 
 void input() {
+	while (true)
+	{
+		cout << "Masukan panjang element array: ";
+		cin >> n;
+
+		if (n <= 20)
+			break;
+		else
+			cout << "\nMaksimum panjang array adalah 20" << endl;
+	}
 	
 }
